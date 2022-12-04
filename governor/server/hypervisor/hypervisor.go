0502fd228b887098a2f34ca5c1509f19b482b3dc
Minke Zhang <minke.zhang@gmail.com>
2022-12-04 23:09:48 +0000

    Add hypervisor interface

---
+++ governor/server/hypervisor/hypervisor.go
@@ -0,0 +1,18 @@
+package hypervisor
+
+import (
+	"context"
+)
+
+type H interface {
+	// RequestAllocateGPU returns a list of currently free GPU IDs. These
+	// GPUs may be allocated between this call and ReserveGPU.
+	RequestAllocateGPU(int n) []int
+
+	// ReserveGPU will lock a GPU as used. FreeGPU must be called upon a
+	// context cancel. The method returns false if the GPU is already in
+	// use.
+	ReserveGPU(ctx context.Context, int n) bool
+
+	FreeGPU(int n) bool
+}
